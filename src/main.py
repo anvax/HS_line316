05167629f53a407be319d73997513cf110dea03b
anvax <andreykhudobin23@gmail.com>
2024-05-07 14:10:23 +0300

    opc ua

--- src/main.py
+++ src/main.py
@@ -1,49 +1,40 @@
-from logic_procs import *
-from logic_hs import *
-from logic_packs import *
-from logic_ss import *
+import sys
+from line316 import *
+from opcua import ua, Server, Client
+
 
 
 def main():
-    procs = ProcS
-    hs = HS
-    packs = PackS
-    ss = SS
-    hs.pushObject()
-    hs.gripperOpen()
-    hs.gripperDown()
-    hs.gripperClose()
-    hs.gripperUp()
-    hs.gripperMoveLeft()
-    hs.gripperDown()
-    hs.gripperOpen()
-    hs.gripperUp()
-    hs.gripperClose()
-    procs.spin()
-    procs.pushColor()
-    procs.spin()
-    procs.drill()
-    procs.spin()
-    hs.gripperOpen()
-    hs.gripperDown()
-    hs.gripperClose()
-    hs.gripperUp()
-    hs.moveToPackS()
-    hs.gripperDown()
-    hs.gripperOpen()
-    hs.gripperUp()
-    hs.gripperClose()
-    packs.pack()
-    hs.gripperOpen()
-    hs.gripperDown()
-    hs.gripperClose()
-    hs.gripperUp()
-    hs.moveRight()
-    hs.gripperDown()
-    hs.gripperOpen()
-    hs.gripperUp()
-    hs.gripperClose()
-    ss.sort()
+    HSline316 = HSline
+    client = Client("opc.tcp://localhost:4840/freeopcua/server/")
+
+    try:
+        # Подключаемся к серверу
+        client.connect()
+
+        # Получаем объект Node для тэга, который хотим изменить
+        node = client.get_node("ns=2;i=2")  # Пример адреса тэга
+        # Считываем значение тэга
+        value = node.get_value()
+
+        #HSline316.start(куча тегов)
+
+
+        # Записываем новое значение тэга
+        node.set_value(False)
+
+        # Считываем и выводим новое значение тэга
+        new_value = node.get_value()
+        print(f"Новое значение тэга: {new_value}")
+
+        # Другие операции с тэгами...
+
+    finally:
+        # Отключаемся от сервера
+        client.disconnect()
+
+
+
 
 
 if __name__ == '__main__':
