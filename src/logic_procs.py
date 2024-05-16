19fd13644cd641f0f8ee1f25450e6902590091a6
anvax <andreykhudobin23@gmail.com>
2024-05-16 15:37:22 +0300

    logic procs

--- src/logic_procs.py
+++ src/logic_procs.py
@@ -3,20 +3,58 @@ from opc_ua_operations import *
 
 class ProcS:
     client = None
+    color = None
+    counter = 0
+    temp=0
+    #Input tags:
+    CarouselRotation = None
+    M5 = None
+    ColorDetection = None
+    RedAndSilvery = None
+    Silvery = None
 
-    def __init__(self, client, tag1):
+    #Output tags:
+    #...
+
+    def __init__(self, client, tag1, tag2,tag3,tag4,tag5):
         self.client = client
-        self.tag1 = tag1
+        self.CarouselRotation = tag1
+        self.M5 = tag2
+        self.ColorDetection=tag3
+        self.RedAndSilvery=tag4
+        self.Silvery=tag5
         print("ProcS created")
 
     @classmethod
-    def spin(cls):
-        write_value_bool('"ns=3;s="Top_secret"."empty"', True, cls.client)
+    def start(cls):
+        #spin
+        write_value_bool()
+        if cls.CarouselRotation:
+            cls.counter += 1
+            if cls.ColorDetection:
+                # m5 down
+                write_value_bool()
+                if cls.M5:
+                    pass
+                    # there are hole in object
+                else:
+                    pass
+                    # no hole
+                # m5 up
+                write_value_bool()
+                if cls.RedAndSilvery and cls.Silvery:
+                    cls.color = "silver"
+                else:
+                    if cls.RedAndSilvery:
+                        cls.color = "red"
+                    else:
+                        cls.color = "black"
+            if cls.counter == 5:
+                # drill
+                write_value_bool()
+        if cls.counter == 6:
+            cls.temp=1
+            cls.counter = 0
+            # no spin
+            write_value_bool()
 
-    @classmethod
-    def pushColor(cls):
-        pass
-
-    @classmethod
-    def drill(cls):
-        pass
--- src/main.py
+++ src/main.py
@@ -1,19 +1,45 @@
 import sys
-from line316 import *
+#from line316 import *
+from logic_hs import *
+from logic_ss import *
+from logic_procs import *
+from logic_packs import *
 from threading import Thread
 from opc_ua_operations import *
 from opcua import ua, Server, Client
+tag1 = None
+tag2 = None
+tag3 = None
+tag4 = None
+tag5 = None
+step = None
+client = Client("opc.tcp://192.168.3.11:5005")
+
+
+def update_tags():
+    while True:
+        step = read_input_value('"fref"', client)
+        tag1 = read_input_value('"ns=3;s="Top_secret"."empty"', client)
+        tag2 = read_input_value('"ns=4;s="Top_secret"."empty"', client)
+        tag3 = read_input_value('"ns=5;s="Top_secret"."empty"', client)
+        tag4 = read_input_value('"ns=6;s="Top_secret"."empty"', client)
+        tag5 = read_input_value('"ns=7;s="Top_secret"', client)
+        # ...other tags
+def procs():
+    while step==1:
+        while not procs.temp:
+            procs.start()
+
 
 if __name__ == '__main__':
 
-    client = Client("opc.tcp://192.168.3.11:5005")
-    tag1 = None
-    tag2 = None
-    tag3 = None
-    tag4 = None
     try:
         # Подключаемся к серверу
         client.connect()
+        procs = ProcS(client, tag1, tag2, tag3, tag4, tag5)
+        hs = HS(client, tag2)
+        packs = PackS(client, tag3)
+        ss = SS(client, tag4)
         HSline316 = HSline(client, tag1, tag2, tag3, tag4)
         t1 = Thread(target=HSline316.update_tags())
         t1.start()
