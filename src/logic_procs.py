da5b7a2ce79a3654671c1bdaf94908e644acea53
Timur <Timur.252002@gmail.com>
2024-05-16 17:34:27 +0300

    Test

--- src/logic_procs.py
+++ src/logic_procs.py
@@ -1,60 +1,64 @@
+from opc_ua_operations import client
 from opc_ua_operations import *
 
 
 class ProcS:
-    client = None
     color = None
     counter = 0
-    temp=0
-    #Input tags:
+    temp = 0
+    # Input tags:
     CarouselRotation = None
     M5 = None
     ColorDetection = None
     RedAndSilvery = None
     Silvery = None
 
-    #Output tags:
-    #...
+    # Output tags:
+    Spin = None
 
-    def __init__(self, client, tag1, tag2,tag3,tag4,tag5):
-        self.client = client
+    def __init__(self, tag1, tag2, tag3, tag4, tag5, tag6):
         self.CarouselRotation = tag1
         self.M5 = tag2
-        self.ColorDetection=tag3
-        self.RedAndSilvery=tag4
-        self.Silvery=tag5
+        self.ColorDetection = tag3
+        self.RedAndSilvery = tag4
+        self.Silvery = tag5
+        self.Spin = tag6
         print("ProcS created")
 
     @classmethod
     def start(cls):
-        #spin
-        write_value_bool()
-        if cls.CarouselRotation:
-            cls.counter += 1
-            if cls.ColorDetection:
-                # m5 down
-                write_value_bool()
-                if cls.M5:
-                    pass
-                    # there are hole in object
-                else:
-                    pass
-                    # no hole
-                # m5 up
-                write_value_bool()
-                if cls.RedAndSilvery and cls.Silvery:
-                    cls.color = "silver"
-                else:
-                    if cls.RedAndSilvery:
-                        cls.color = "red"
-                    else:
-                        cls.color = "black"
-            if cls.counter == 5:
-                # drill
-                write_value_bool()
-        if cls.counter == 6:
-            cls.temp=1
-            cls.counter = 0
-            # no spin
-            write_value_bool()
+        # spin
+        try:
+            write_value_bool("ns=4;i=11", False)
 
+            if cls.CarouselRotation:
+                cls.counter += 1
+                if cls.ColorDetection:
+                    # m5 down
+                    write_value_bool()
+                    if cls.M5:
+                        pass
+                        # there are hole in object
+                    else:
+                        pass
+                        # no hole
+                    # m5 up
+                    write_value_bool()
+                    if cls.RedAndSilvery and cls.Silvery:
+                        cls.color = "silver"
+                    else:
+                        if cls.RedAndSilvery:
+                            cls.color = "red"
+                        else:
+                            cls.color = "black"
+                if cls.counter == 5:
+                    # drill
+                    write_value_bool()
+            if cls.counter == 6:
+                cls.temp = 1
+                cls.counter = 0
+                # no spin
+                write_value_bool()
+        except Exception as e:
+            # print(e)
+            pass
--- src/main.py
+++ src/main.py
@@ -1,34 +1,38 @@
 import sys
-#from line316 import *
+# from line316 import *
 from logic_hs import *
 from logic_ss import *
 from logic_procs import *
 from logic_packs import *
 from threading import Thread
+from opc_ua_operations import client
 from opc_ua_operations import *
 from opcua import ua, Server, Client
+
 tag1 = None
 tag2 = None
 tag3 = None
 tag4 = None
 tag5 = None
-step = None
-client = Client("opc.tcp://192.168.3.11:5005")
+tag6 = None
+step = 1
 
+
+client
 
 def update_tags():
     while True:
-        step = read_input_value('"fref"', client)
-        tag1 = read_input_value('"ns=3;s="Top_secret"."empty"', client)
-        tag2 = read_input_value('"ns=4;s="Top_secret"."empty"', client)
-        tag3 = read_input_value('"ns=5;s="Top_secret"."empty"', client)
-        tag4 = read_input_value('"ns=6;s="Top_secret"."empty"', client)
-        tag5 = read_input_value('"ns=7;s="Top_secret"', client)
+        # step = read_input_value('"fref"', client)
+        tag6 = read_input_value('ns=4;i=11')
         # ...other tags
-def procs():
-    while step==1:
+
+
+def process():
+    global step
+    while step == 1:
         while not procs.temp:
             procs.start()
+            step = 2
 
 
 if __name__ == '__main__':
@@ -36,15 +40,22 @@ if __name__ == '__main__':
     try:
         # Подключаемся к серверу
         client.connect()
-        procs = ProcS(client, tag1, tag2, tag3, tag4, tag5)
+        node = client.get_node('ns=4;i=11')
+        print(node.server)
+        print(node.nodeid)
+        procs = ProcS(tag1, tag2, tag3, tag4, tag5, tag6)
         hs = HS(client, tag2)
         packs = PackS(client, tag3)
         ss = SS(client, tag4)
-        HSline316 = HSline(client, tag1, tag2, tag3, tag4)
-        t1 = Thread(target=HSline316.update_tags())
+
+        t1 = Thread(target=update_tags)
+        # t2 = Thread(target=process)
         t1.start()
-        t2 = Thread(target=HSline316.start())
-        t2.start()
+        # t2.start()
+    except Exception as e:
+        print(2)
+        print(e)
+        # print(1)
     finally:
         # Отключаемся от сервера
         client.disconnect()
